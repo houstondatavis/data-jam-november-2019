60b8d0d0eb5ba95c4e0d8ef804c810fd574356c0
Ben H-M <bhh1@rice.edu>
2019-11-23 11:58:35 -0600

    Added my ish

---
+++ R/1-clean_data.R
@@ -0,0 +1,44 @@
+# Load libraries
+library(dplyr)
+library(jsonlite)
+
+# Read in revenue data
+rev <- read.csv("revenue_details.csv", stringsAsFactors = FALSE)
+
+# Read in trip data
+trips <- read.csv("trip_data.csv", stringsAsFactors = FALSE)
+
+# Filter
+trips <- trips %>%
+  filter(CheckoutKioskName != "" & ReturnKioskName != "") %>%
+  filter(DurationMins > 1) %>%
+  filter(UserRole != "Maintenance") 
+
+# Nodes
+stations <- unique(c(trips$Checkout.Kiosk, trips$Return.Kiosk))
+
+data.frame(
+    id = stations,
+    group = 1
+  ) %>%
+  jsonlite::toJSON(auto_unbox=T) %>% 
+  write("nodes-data.json")
+
+# Links
+trips %>%
+  group_by(Checkout.Kiosk, Return.Kiosk) %>%
+  summarise(
+    num_trips = n(),
+    dist_traveled = sum(Distance),
+    carbon_saved = sum(EstimatedCarbonOffset),
+    calories_burned = sum(EstimatedCaloriesBurned),
+    time_traveled = sum(DurationMins)
+  ) %>%
+  rename(
+    source = Checkout.Kiosk,
+    target = Return.Kiosk
+  ) %>%
+  select(source, target, num_trips) %>%
+  jsonlite::toJSON(auto_unbox=T) %>% 
+  write("links-data.json")
+
