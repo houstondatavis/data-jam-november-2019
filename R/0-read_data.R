2c9d8699f91a26621509a12ae16e9c4f7045c360
neerajt <neerajtandon25@gmail.com>
2019-11-23 09:44:30 -0600

    update read-data

--- R/0-read_data.R
+++ R/0-read_data.R
@@ -38,9 +38,10 @@ trip_data <- rbind(pre_change, post_change, fill=TRUE)
 
 fwrite(trip_data, 'trip_data.csv')
 
-file_paths <- list.files(path = "src/Revenue Data/Revenue Details By Year",
+#file_paths <- list.files(path = "src/Revenue Data/Revenue Details By Year",
 			 pattern = ".csv",
 			 full.names=TRUE)
-revenue_details <- do.call(rbind, lapply(file_paths, fread))
 
-fwrite(revenue_details, 'revenue_details.csv')
+#revenue_details <- print(do.call(dim, lapply(file_paths, fread)))
+
+#fwrite(revenue_details, 'revenue_details.csv')
